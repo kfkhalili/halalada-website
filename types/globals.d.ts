fdece4ccf2e3893136c4b9e13cefc1ad95120b34
Khalid Alkhalili <kfkhalili@hotmail.com>
2024-12-01 15:50:14 +0100

    chore: remove unused types

--- types/globals.d.ts
+++
@@ -1,6 +0,0 @@
-// Extend the Window interface to include the cardano property
-interface Window {
-  cardano?: {
-    [key: string]: any; // Replace `any` with specific wallet API types if known
-  };
-}
